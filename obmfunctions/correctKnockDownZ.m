fbe410b9a546bd96ee7959f311e22cca49c05e60
olavobm <olavobm@gmail.com>
2017-01-30 15:45:07 -0800

    first commit.

---
+++ obmfunctions/correctKnockDownZ.m
@@ -0,0 +1,14 @@
+function dataCorrected = correctKnockDownZ(interpObj, moorsensors, editedData)
+% dataCorrected = CORRECTKNOCKDOWNZ(interpObj, moorsensors, editedData)
+%
+%   inputs:
+%       - interpObj:
+%       - moorsensors:
+%       - editedData:
+%
+%   outputs:
+%       - dataCorrected: data corrected by mooring knockdown.
+%
+% Function CORRECTKNOCKDOWNZ
+%
+% Olavo Badaro Marques, 30/Jan/2017.
