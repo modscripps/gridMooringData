d1d28f9a0f6349645874f86f1d589377700a1f36
olavobm <olavobm@gmail.com>
2017-03-30 17:26:48 -0700

    updated to deal with instruments of the same type that have different fields.

--- template/templateMooringWorksheet.m
+++ template/templateMooringWorksheet.m
@@ -162,10 +162,40 @@ for i1 = 1:length(instrmntTypes)
     for i2 = 1:size(moorsensors.(instrmntTypes{i1}), 1)
         
         % Call the load_mooring_datafile function:
-        loadedData.(instrmntTypes{i1})(i2) = ...
-                   loadMooringData(moordir, ...
+        auxDataStruct = loadMooringData(moordir, ...
                                    instrmntTypes{i1}, ...
                                    moorsensors.(instrmntTypes{i1})(i2, 1));
+        
+        % Assign auxDataStruct to loadedData:
+        if i2 == 1
+        % Assign right away:
+        
+            loadedData.(instrmntTypes{i1})(i2) = auxDataStruct;
+            
+        else
+        % Make sure the new structure has the same fields as
+        % the existing array. Add fields with empty content
+        % if the field names don't match:
+            
+            auxFieldNames = sort(fieldnames(auxDataStruct));
+            datFieldNames = sort(fieldnames(loadedData.(instrmntTypes{i1})));
+            
+            if ~isequal(auxFieldNames, datFieldNames);
+                
+                loadedData.(instrmntTypes{i1}) = ...
+                        matchStructArray(loadedData.(instrmntTypes{i1})(1:(i2-1)), ...
+                                         auxDataStruct);
+            else
+
+                auxDataStruct = orderfields(auxDataStruct, ...
+                               fieldnames(loadedData.(instrmntTypes{i1})));
+                    
+                loadedData.(instrmntTypes{i1})(i2) = auxDataStruct;
+                
+            end
+            
+        end
+        
     end
     
 end
